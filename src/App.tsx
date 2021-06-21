4dc9f139dbff901e6fb0f028b4d0c1241f1f9eaf
miamiultras <zurawinaiserow@gmail.com>
2021-06-21 21:17:27 +0000

    fetch data with useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,52 @@
-import { useEffect, useState } from "react";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+  useQuery,
+} from "@apollo/client";
+import "./App.css";
 
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql",
   cache: new InMemoryCache(),
 });
 
-function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    async function fetchData() {
-      const { data, loading } = await client.query({
-        query: gql`
-          query {
-            characters(page: 2, filter: { name: "rick" }) {
-              info {
-                count
-              }
-              results {
-                name
-              }
-            }
-            location(id: 1) {
-              id
-            }
-            episodesByIds(ids: [1, 2]) {
-              id
-            }
-          }
-        `,
-      });
-      setData(data);
-      setLoading(loading);
+const RICKS = gql`
+  query {
+    characters(page: 2, filter: { name: "rick" }) {
+      info {
+        count
+      }
+      results {
+        name
+      }
     }
-    fetchData();
-  }, []);
+  }
+`;
+
+function Ricks() {
+  const { loading, error, data } = useQuery(RICKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const { characters } = data;
+  return characters.results.map(({ name }: { name: string }, index: number) => (
+    <div key={index}>
+      <p>{name}</p>
+    </div>
+  ));
+}
+
+function App() {
   return (
-    <>{loading ? "Loading..." : <pre>{JSON.stringify(data, null, 4)}</pre>}</>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h2>MortyQL 🚀</h2>
+        <Ricks />
+      </div>
+    </ApolloProvider>
   );
 }
 
