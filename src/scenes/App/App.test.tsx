1d4046b93f2fce5d8cb92f11736cd6d8b6ac47ae
miamiultras <zurawinaiserow@gmail.com>
2021-06-25 18:37:15 +0000

    fix test

--- src/scenes/App/App.test.tsx
+++ src/scenes/App/App.test.tsx
@@ -1,11 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+
 import App from "./App";
 
 test("renders title", () => {
   render(
-    <App>
-      <div>children</div>
-    </App>
+    <MemoryRouter>
+      <App>
+        <div>children</div>
+      </App>
+    </MemoryRouter>
   );
   expect(screen.getByText("mortyQL 🧪")).toBeInTheDocument();
   expect(screen.getByText("children")).toBeInTheDocument();
