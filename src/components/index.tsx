35c497fdde309851bd85e4e1bb6909af275cdb34
miamiultras <zurawinaiserow@gmail.com>
2021-06-28 20:59:36 +0000

    add search component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export * from "./card";
 export * from "./navbar";
+export * from "./search";
 export * from "./spinner";
---
+++ src/components/search/search.tsx
@@ -0,0 +1,39 @@
+import React, { SetStateAction, useState } from "react";
+import styles from "./search.module.scss";
+
+type SearchProps = {
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+};
+
+function debounce<T>(callback: (arg: T) => void, wait: number) {
+  let timeout: NodeJS.Timeout;
+  return (arg: T) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => callback(arg), wait);
+  };
+}
+
+export function Search({ setQuery }: SearchProps) {
+  const [value, setValue] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setValue(value);
+    handleSearch(value);
+  };
+
+  const handleSearch = debounce<SetStateAction<string>>((value) => {
+    setQuery(value);
+  }, 500);
+
+  return (
+    <div className={styles.searchWrapper}>
+      <input
+        placeholder="Search..."
+        className={styles.search}
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
