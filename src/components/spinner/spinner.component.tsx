9e62164cdffc07953422944fd8deeaba32c39582
miamiultras <zurawinaiserow@gmail.com>
2021-07-06 18:09:27 +0000

    refactor spinner

--- src/components/spinner/spinner.component.tsx
+++ src/components/spinner/spinner.component.tsx
@@ -1,9 +1,5 @@
 import styles from "./spinner.module.scss";
 
 export function Spinner() {
-  return (
-    <div data-testid="spinner" className={styles.spinnerWrapper}>
-      <div className={styles.spinner} />
-    </div>
-  );
+  return <div data-testid="spinner" className={styles.spinner} />;
 }
