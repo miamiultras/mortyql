e208f3cfe2fa3d7ad274d5681df976fedba91f21
miamiultras <zurawinaiserow@gmail.com>
2021-07-06 18:59:18 +0000

    add intersection observer hook util

---
+++ src/lib/use-intersect.hook.ts
@@ -0,0 +1,31 @@
+import { useState, useRef, useEffect } from "react";
+
+export const useIntersect = (
+  { root = null, rootMargin = "0px", threshold = 0 },
+  callback: () => void
+) => {
+  const [node, setNode] = useState<any>(null);
+  const observer = useRef(
+    new window.IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) callback();
+      },
+      {
+        root,
+        rootMargin,
+        threshold,
+      }
+    )
+  );
+
+  useEffect(() => {
+    const { current: currentObserver } = observer;
+    currentObserver.disconnect();
+
+    if (node) currentObserver.observe(node);
+
+    return () => currentObserver.disconnect();
+  }, [node]);
+
+  return setNode;
+};
